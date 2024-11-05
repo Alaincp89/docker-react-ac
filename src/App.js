57d3fc04f2a3f0593d6a1800aeffc98f6c3254b3
Ing. Alain Cervantes <alaincervantesp@gmail.com>
2024-11-05 14:42:37 -0500

    modified project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenido al Curso de Docker y Kubernetes
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
